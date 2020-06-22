cc2e1db2cc677f3d23a8d3e94e03f7d6b8534d1e
Michael Ploughman <michaelhploughman@gmail.com>
2020-06-22 15:31:21 -0600

    API : setup model for profiles

---
+++ src/profiles/profile-model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+  basicInfo: {
+    name: String,
+    sports: Array,
+    nationality: String,
+    gender: String,
+    dob: Date,
+  },
+  about: {
+    description: String,
+    location: String,
+    team: String,
+    association: String,
+    interests: String,
+    charities: String,
+    pets: String,
+    married: String,
+    drinks: String,
+  },
+  socialMedia: {
+    facebook: String,
+    twitter: String,
+    instagram: String,
+    youtube: String,
+    twitch: String,
+    snapchat: String,
+  },
+});
+
+mongoose.model('Profile', profileSchema);
