30bddada068af6c62030bc611c9863cef096906b
Michael Ploughman <michael@michaelhploughman.com>
2020-06-28 11:51:43 -0600

    API : mongodb connection setup

--- api/src/profiles/profiles-route.js
+++ api/src/profiles/profiles-route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Profile = mongoose.model('Profile');
+const profileSchema = require('./profile-model');
+const Profile = mongoose.model('Profile', profileSchema);
 
 const profilesRouter = express.Router();
 const jsonBodyParser = express.json();
