53c7c9cb9dad98e398b86f35df13abf9ca01f41f
Michael Ploughman <michael@michaelhploughman.com>
2020-07-06 20:56:29 -0600

    Fixed checkboxes

--- client/app/app.js
+++ client/app/app.js
@@ -2,43 +2,48 @@
 
 // Declare app level module which depends on views, and core components
 const athleteProfile = angular
-  .module("athleteProfile", [
-    "ui.router",
-    "ngAnimate",
-    "ui.bootstrap",
-  ])
+  .module("athleteProfile", ["ui.router", "ngAnimate", "ui.bootstrap"])
   .config(function ($urlRouterProvider, $stateProvider) {
-      $stateProvider.state('profile', {
-        url: '/profile',
-        templateUrl: 'form.html',
-        controller: 'FormCtrl'
+    $stateProvider
+      .state("profile", {
+        url: "/profile",
+        templateUrl: "form.html",
+        controller: "FormCtrl",
       })
-      .state('profile.basicInfo', {
-        url: '/basic-info',
-        templateUrl: './views/basicInfo/basicInfo.html'
+      .state("profile.basicInfo", {
+        url: "/basic-info",
+        templateUrl: "./views/basicInfo/basicInfo.html",
       })
-      .state('profile.about', {
-        url: '/about',
-        templateUrl: './views/about/about.html'
-      }).state('profile.socialMedia', {
-        url: '/social-media',
-        templateUrl: './views/socialMedia/socialMedia.html'
+      .state("profile.about", {
+        url: "/about",
+        templateUrl: "./views/about/about.html",
+      })
+      .state("profile.socialMedia", {
+        url: "/social-media",
+        templateUrl: "./views/socialMedia/socialMedia.html",
+      })
+      .state("profile.summary", {
+        url: "/summary",
+        templateUrl: "views/summary/summary.html",
+      })
+      .state("profile.profileList", {
+        url: "/profile-list",
+        templateUrl: "./views/profileList/profileList.html",
       });
 
-      $urlRouterProvider.otherwise('/profile/basic-info');
-    })
+    $urlRouterProvider.otherwise("/profile/basic-info");
+  })
   .controller("DropdownController", DropdownController)
-  .controller("FormCtrl", FormCtrl)
+  .controller("FormCtrl", FormCtrl);
 
 function FormCtrl($scope) {
   $scope.newProfile = {};
 
-  $scope.processForm = function() {
-    alert('Successfully submitted profile!');
+  $scope.processForm = function () {
+    alert("Successfully submitted profile!");
   };
 }
 
 function DropdownController($scope) {
   $scope.isNavCollapsed = true;
 }
-
