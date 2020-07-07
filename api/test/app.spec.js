2df1c153e37368fe20eb6b476783d7609aa789ec
Michael Ploughman <michael@michaelhploughman.com>
2020-07-06 21:28:03 -0600

    CRemove unused backend code

--- api/test/app.spec.js
+++
@@ -1,9 +0,0 @@
-const app = require('../src/app')
-
-describe('/api/profiles', () => {
-  it('GET /api/profiles responds with 200', () => {
-    return supertest(app)
-      .get('/api/profiles')
-      .expect(200)
-  })
-})
